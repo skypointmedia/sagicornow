b01bf2866ffd6436b4a981c3a2ab1f410c39e8bf
Curtis Jackson <curtis.jackson@syntaxja.com>
2019-02-28 08:58:05 -0500

    database setup

---
+++ Data/Entities/ProposalHistory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SagicorNow.Data.Entities
+{
+    public class ProposalHistory
+    {
+        public ProposalHistory()
+        {
+            CreatedDateTime = DateTime.Now;
+            LastActiveDateTime = DateTime.Now;
+        }
+
+        [Key,StringLength(9)]
+        public string SSN { get; set; }
+        public string ActivityId { get; set; }
+        public DateTime  CreatedDateTime { get; set; }
+        public DateTime LastActiveDateTime { get; set; }
+    }
+}
---
+++ Data/SageNowContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using SagicorNow.Data.Entities;
+
+namespace SagicorNow.Data
+{
+    public class SageNowContext: DbContext{
+        public SageNowContext() : base("SagicorNow")
+        {
+        }
+        public IDbSet<ProposalHistory> ProposalHistories { get; set; }
+    }
+}
